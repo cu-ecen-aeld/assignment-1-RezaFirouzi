7de46b5df5f6d98e3baecbf3fe6cf62ee55f027e
Reza <sr.firouzi@gmail.com>
2024-09-21 09:30:43 +0330

    first working implementation of finder.sh

---
+++ finder-app/finder.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Author : Reza Firouzi
+
+set -e
+
+if [ ! $# -eq 2 ]; then
+    echo "The two required arguments has not been passed to the script!"
+    exit 1
+elif [ ! -d $1 ]; then
+    echo "The first argument does not exist or is not a directory"
+    exit 1
+fi
+
+FILESDIR=$1
+SEARCHSTR=$2
+
+#FILESLIST=$(find $FILESDIR -type f)
+
+#NUMMATCH=$(grep $SEARCHSTR $FILESLIST) | wc -l
+#NUMFILES=$(wc -l $FILESLIST)
+NUMFILES=$(find $FILESDIR -type f | wc -l)
+NUMMATCH=$(grep $SEARCHSTR $(find $FILESDIR -type f) | wc -l)
+
+echo "The number of files are $NUMFILES and the number of matching lines are $NUMMATCH"
+exit 0
+
