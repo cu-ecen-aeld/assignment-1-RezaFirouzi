1fa5112e19b34f8787525387e2f8109fb95c81a4
Reza <sr.firouzi@gmail.com>
2024-09-24 16:06:31 +0330

    Applied a review comment - free allocated memory

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,10 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_EQUAL_STRING(my_username(),malloc_username_from_conf_file());
+    const char * userName = my_username();
+    char * userNameFromConfFile = malloc_username_from_conf_file();
+
+    TEST_ASSERT_EQUAL_STRING(userName,userNameFromConfFile);
+
+    free(userNameFromConfFile);
 }
